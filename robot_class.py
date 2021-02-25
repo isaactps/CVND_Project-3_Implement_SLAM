59dba8dbb7f569c4db20a5c7b79f6078f2c8dff1
Isaac Tan <isaac_tps@yahoo.com>
2021-02-25 15:49:57 -0800

    Uploaded my implementation files for the Project

--- robot_class.py
+++ robot_class.py
@@ -91,8 +91,14 @@ class robot:
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
         
         ## TODO: return the final, complete list of measurements
+        for index, landmark in enumerate(self.landmarks):
+            dx = landmark[0] - self.x + self.rand()*self.measurement_noise
+            dy = landmark[1] - self.y + self.rand()*self.measurement_noise
+            
+            if ((abs(dx) <= self.measurement_range) and (abs(dy) <= self.measurement_range)):
+                measurements.append([index, dx, dy])        
+        
         return measurements
-
 
     # --------
     # make_landmarks:
